6ddee0210d1bfe514ea9bcf964d81d370ba3fbfe
Ian Ybarra <50596097+IanYbarra2000@users.noreply.github.com>
2021-04-03 15:00:04 -0500

    add most if not all functionality

--- HW7.py
+++ HW7.py
@@ -1,28 +1,134 @@
 import pickle
-import nltk
-from nltk.util import ngrams
 import random
+import nltk
+import sys
+from nltk.util import ngrams
 from nltk import word_tokenize
 from nltk import WordNetLemmatizer
-#from chatterbot import ChatBot
-#from chatterbot.conversation import Statement
-#from chatterbot.trainers import ListTrainer
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def searchOnMiss(tkn):
+    """This function is called when a match to a specific keyword cannot be found and uses cosine similarity on a tfidf vector to suggest a possible match
+
+    Args:
+        tkn ([string]): tokens taken from the user input
+    """
+
+    cosine = dict()
+
+    reqVocab = set(tkn)
+    vocabDict = dict()
+
+    tfidfV=TfidfVectorizer()
+    txt=' '.join(tkn)
+
+    for x in knowledge.keys():
+        ktxt = ' '.join(knowledge[x])
+        vm = tfidfV.fit_transform([txt,ktxt])
+        csm = cosine_similarity(vm)
+        cosine[x] = csm[0,1]
+    max = 0
+    maxI = None
+    for x in cosine.keys():
+        if(cosine[x]>max and x not in UserInfo['history'].keys()):
+            max=cosine[x]
+            maxI=x
+    print("I'm not exactly sure what you mean. Were you refering to",maxI+"?")
+    YoN = input(name+": ")
+    if(yesOrNo(YoN)):
+        updateHistory(maxI)
+    
+    
+
+def processRequest(req):
+    """Takes the user's input(or request) and returns tokens and lemmas from said text input
+
+    Args:
+        req (string): the user's input
+
+    Returns:
+        ([string],[string]): a tuple containing the tokens and lemmas
+    """
+    tokens = nltk.word_tokenize(req.lower())
+    
+    ####
+    stopWords = set(stopwords.words('english'))
+    tokens = [t for t in tokens if not t in stopWords]
+    ####
+    lemmatizer = WordNetLemmatizer()
+    lems = [lemmatizer.lemmatize(t) for t in tokens]
+    return (lems,tokens)
 def matchPhrase(tkn):
+    """can get a partial match on an input or match based on multiple words
+
+    Args:
+        tkn ([string]): tokens taken from the user input
+
+    Returns:
+        [string]: the key of that is found to match the input
+    """
+
+    #tries to find match three times
+    #starts with trying to match trigrams as to avoid returning a partial match incorrectly
+    #requirements to match get less demanding with each set of for loops
+    trigram = list(ngrams(tkn,3))
     for k in knowledge.keys():
         t = nltk.word_tokenize(k.lower())
-        for kt in t:
-            if(kt in tkn):
+        stopWords = set(stopwords.words('english'))#
+        t = [s for s in t if not s in stopWords]#
+        ktrigram = list(ngrams(t,3))
+        for kb in ktrigram:
+            if(kb in trigram):
                 return k
     bigram = list(ngrams(tkn,2))
     for k in knowledge.keys():
         t = nltk.word_tokenize(k.lower())
+        stopWords = set(stopwords.words('english'))#
+        t = [s for s in t if not s in stopWords]#
         kbigram = list(ngrams(t,2))
         for kb in kbigram:
             if(kb in bigram):
                 return k
+    for k in knowledge.keys():
+        #print(k)
+        t = nltk.word_tokenize(k.lower())
+        stopWords = set(stopwords.words('english'))#
+        t = [s for s in t if not s in stopWords]#
+        for kt in t:
+            if(kt in tkn):
+                #print(kt,'in',tkn)
+                return k
+    
     return None
+def end():
+    print("Brando: Thanks for using the chatbot. Goodbye, "+name+".")
+    sys.exit()
+def yesOrNo(txt):
+    """processes yes or no responses
+
+    Args:
+        txt (string): user input
+
+    Returns:
+        boolean: returns true if answer is determined to be yes and no if not
+    """
+    proc = nltk.word_tokenize(txt)
+    y = ['yes','sure','okay','yeah','absolutely','indeed','ya','yup','yep','totally','alright','certainly','definitely','gladly','indubitably','undoubtedly']
+    for w in proc:
+        if(w in y):
+            return True
+    return False
 
 def updateHistory(kToUpdate):
+    """updates the user's history in the user.p file and retrieves relevant information from the knowledge base
+
+    Args:
+        kToUpdate (string): the key that will be used to access information in the knowledge base and that will be entered into the user history
+    """
+
     try:
         callNum=0
         if(kToUpdate in UserInfo['history'].keys()):
@@ -32,42 +138,132 @@ def updateHistory(kToUpdate):
             if(callNum>len(knowledge[kToUpdate])-1): #If a specific keyword has been called more times than the number responses available it cycles back to the beginning while still maintaining a record of the count
                 callNum=callNum%len(knowledge[kToUpdate])
             print("Brando: You've already asked about",kToUpdate+". I will tell you something else about it if I can.")
-            print("Brando:",knowledge[kToUpdate][0][callNum])
+            print("Brando:",knowledge[kToUpdate][callNum])
         else:
             UserInfo['history'][kToUpdate]=0
-            print("Brando:",knowledge[kToUpdate][0][0])
+            print("Brando:",knowledge[kToUpdate][0])
     except Exception as e:
          #stores history as a dictionary in with the keyword as the key and the number of times it has been asked about as the value
         UserInfo['history'] = dict()
         UserInfo['history'][kToUpdate] = 0
 
-        print("Brando:",knowledge[kToUpdate][0][0])
+        print("Brando:",knowledge[kToUpdate][0])
     pickle.dump(UserInfo,open('Users/'+name+'.p','wb'))
 
+    print("Brando: Would you like more information on",kToUpdate+"?")
+    YoN = input(name+": ")
+    if(yesOrNo(YoN)):
+        updateHistory(kToUpdate)
+def getExamples():
+    """gives five random keys from the knowledge base to show the users examples of searches they can perform
 
+    Returns:
+        [string]: list of examples
+    """
+    ex = set()
+    index=0
+    keyArr = list(knowledge.keys())
+    while(len(ex)<=5):
+        index = random.randint(0,len(keyArr)-1)
+        ex.add(keyArr[index])
+    return list(ex)
 
+def synsetMatching(lemma):
+    """determines which topic category the user wants to ask about
 
-knowledge = pickle.load(open('t_knowledgeBase.p','rb'))
-if(False):
+    Args:
+        lemma ([string]): the lemmas from the user's input
 
-    bot = ChatBot('Norman')
+    Returns:
+        string: the tag corresponding to the topic category
+    """
 
-    trainer = ListTrainer(bot)
+    loc = ['location','place','city','country','land','nation','world','planet','site','metropolis','state','area','someplace']
+    char = ['character','person','someone','individual','people']
+    cult = ['culture','civilisation','civilization']
+    era = ['era','event','timeperiod','epoch','battle','timeperiods']
+    mag = ['magic','sorcery','witchcraft','enchantment','conjuration','wizard','necromancy','bewitchment','magical','illusion','spell','investiture']
+    obj = ['object','item','material','substance','ore','fabric','textile']
+    life = ['species','creature','beast','fauna','specie','animal','lifeform','lifeforms']
 
-    #trainer.train("chatterbot.corpus.english")
+    if(lemma in loc):
+        return 'loc'
+    elif(lemma in char):
+        return 'char'
+    elif(lemma in cult):
+        return 'cult'
+    elif(lemma in era):
+        return 'era'
+    elif(lemma in mag):
+        return 'mag'
+    elif(lemma in obj):
+        return 'obj'
+    elif(lemma in life):
+        return 'life'
+    return
+    
+def topicQuestion(tg):
+    """processes questions about specific items once a topic has been selected
 
-    knowledge = pickle.load(open('knowledgeBase.p','rb'))
-    print(knowledge)
-    for topic in knowledge:
-        for r in topic:
-            trainer.train([topic,r])
+    Args:
+        tg (string): the tag corresponding to the topic category
+    """
+    global knowledge
+    knowledge = pickle.load(open('knowledge/'+tg+'_knowledgeBase.p','rb'))
+    topic = None
+    
+    if(tag =='loc'):
 
-    #request = 'Hello'
-    while(True):
-        request = Statement(input('You: '))
-        response = bot.get_response(request)
-        print('bot: ',response)
+        topic = 'location'
+    elif(tag =='char'):
+        topic = 'character'
+    elif(tag =='cult'):
+        topic = 'culture'
+    elif(tag =='era'):
+        topic = 'era or event'
+    elif(tag =='mag'):
+        topic = 'magic or investiture'
+    elif(tag =='obj'):
+        topic = 'object or material'
+    elif(tag =='life'):
+        topic = 'lifeform or creature'
+    examples = getExamples()
+    print("Brando: What",topic,"would you like to know about? (Ex: "+examples[0]+', '+examples[1]+', '+examples[2]+', '+examples[3]+', '+examples[4]+')')
+    req = input(name+": ")
+    lems = processRequest(req)
+    tok = lems[1]
+    lems = lems[0]
+    #print(tok,lems)
+    for x in lems:
+        if(x =='exit' or x=='stop'):
+            end()
+        if(x in knowledge.keys()):
+                keywords.append(x)
+    if(len(keywords)!=0):
+        for x in keywords:
+            updateHistory(x)
+    else:
+        #print('match Phrase')
+        key = matchPhrase(tok)
+        if(key is not None):
+            updateHistory(key)
+        else:
+            searchOnMiss(tok)
+            #print("Brando:","I don't know what you mean")
+    print("Brando: Would you like to ask another question about a",topic+"?")
+    YoN = input(name+": ")
+    if(yesOrNo(YoN)):
+        topicQuestion(tg)
 
+knowledge = dict()
+#char_knowledge=pickle.load(open('char_knowledgeBase.p','rb'))
+#loc_knowledge=pickle.load(open('loc_knowledgeBase.p','rb'))
+#mag_knowledge=pickle.load(open('mag_knowledgeBase.p','rb'))
+#life_knowledge=pickle.load(open('life_knowledgeBase.p','rb'))
+#obj_knowledge=pickle.load(open('obj_knowledgeBase.p','rb'))
+#era_knowledge=pickle.load(open('era_knowledgeBase.p','rb'))
+#print(era_knowledge['Battle of Alta'])
+#cult_knowledge=pickle.load(open('cult_knowledgeBase.p','rb'))
 #TO DO:
 #-Store more personal information than name
 #-Think of how to incorporate that info into responses
@@ -80,35 +276,35 @@ print("Brando: What is your name?")
 name = input("You: ")
 try:
     UserInfo = pickle.load(open('Users/'+name+'.p','rb'))
-    print("Brando: Welcome back",name+".\nBrando: Please ask your questions.")
+    print("Brando: Welcome back",name+".")
 except:
     UserInfo['name'] = name
 
     pickle.dump(UserInfo,open('Users/'+name+'.p','wb'))
 
-    print("Brando: Hello,",name+". I am Brando, a chatbot to assist with basic questions about the lore of the Cosmere.\nBrando: Please ask your questions.")
+    print("Brando: Hello,",name+". I am Brando, a chatbot to assist with basic questions about the lore of the Cosmere.")
+
 
 while(True):
+    print("Brando: Please choose a topic area you are interested in learning more about. Said topic areas include characters, cultures, events or timeperiods, creatures or lifeforms, locations, magic, or objects and materials.")
     request = input(name+": ")
 
     #preprocessing
-    tokens = nltk.word_tokenize(request.lower())
-    lemmatizer = WordNetLemmatizer()
-    lemmas = [lemmatizer.lemmatize(t) for t in tokens]
+    lemmas = processRequest(request)
+    token = lemmas[1]
+    lemmas = lemmas[0]
 
 
-    keywords=[] 
+    keywords=[]
+    tag = None
     for x in lemmas:
-        if(x in knowledge.keys()):
-            keywords.append(x)
-    if(len(keywords)!=0):
-        for x in keywords:
-            updateHistory(x)
+        if(x =='exit' or x=='stop'):
+            end()
+        tag = synsetMatching(x)
+        if(tag):
+            break
+    if(tag is None):
+        print("Brando:","I don't know what you mean")
     else:
-        key = matchPhrase(tokens)
-        if(key is not None):
-            updateHistory(key)
-        else:
-            print("Brando:","I don't know what you mean")
-
-
+        topicQuestion(tag)
+        
--- KBEdittor.py
+++ KBEdittor.py
@@ -5,18 +5,50 @@ from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 import re
 
-knowledge = pickle.load(open('knowledgeBase.p','rb'))
+char_knowledge = dict()
+loc_knowledge = dict()
+mag_knowledge = dict()
+life_knowledge = dict()
+obj_knowledge = dict()
+era_knowledge = dict()
+cult_knowledge = dict()
+
+def scrapeLoc(url):
 
 
+    pg = requests.get(url)
+    strained = SoupStrainer(id='mw-content-text')
+    full = BeautifulSoup(pg.content,'html.parser')
+    s = BeautifulSoup(pg.content,'html.parser',parse_only=strained)
 
+    title = full.find("h1").get_text()
+    reg = re.compile('User:')
+    if(reg.search(title) or title=='Template:Date'):
+        return
+    start_at = s.find(id="toc")
+    if(start_at is not None):
+        target = start_at.find_all_previous("p", limit=2)
+    else:
+        start_at=s.find(id='row-spoiler-notice')
+        target = start_at.find_all_next("p",limit=2)
+    text = ""
+    reg = re.compile('This wiki can now have')
+    for x in target:
+        
+        
+        if(x.get_text()[0]=='“' or reg.search(x.get_text())):
+            #print(x.get_text())
+            continue
+        
+        text +=x.get_text()
+    text=' '.join(text.split())
 
+    text = re.sub(r'[\d\[\]]+', '', text)
+    sents = nltk.sent_tokenize(text)
+    #print(sents)
+    return (title,sents)
 
-def scrapeTxt(url):
-    """takes list of URL's and writes the text from the associated websites to documents
-
-    Args:
-        URL_list (list[string]): list of URLs whose associated webpages are to be scraped
-    """
+def scrapeChar(url):
 
     pg = requests.get(url)
     strained = SoupStrainer(id='mw-content-text')
@@ -28,10 +60,12 @@ def scrapeTxt(url):
     if(start_at is not None):
         target = start_at.find_all_next("p", limit=4)
     else:
-        target = full.find_all("p",limit=2)
+        target = full.find_all("p",limit=4)
     text = ""
+    reg = re.compile('This wiki can now have')
     for x in target:
-        if(x.get_text()[0]=='“' or x.get_text()[0:16]=='This wiki can now'):
+        
+        if(x.get_text()[0]=='“' or reg.search(x.get_text())):
             continue
         
         text +=x.get_text()
@@ -41,8 +75,7 @@ def scrapeTxt(url):
     sents = nltk.sent_tokenize(text)
     return (title,sents)
 
-def scrape_characters():
-    url = 'https://coppermind.net/wiki/Category:Characters'
+def scrape_links(url,type): 
     pg = requests.get(url)
 
     strained = SoupStrainer(id="mw-pages")
@@ -52,9 +85,50 @@ def scrape_characters():
         if(l.get('href')!='None'):
             print("finished",l.get('href'))
             link = "https://coppermind.net"+l.get('href')
-            text = scrapeTxt(link)
-            knowledge[text[0]]=(text[1],'character')
+            if(type=='char'):
+                text = scrapeChar(link)
+                if(text and text[1]):
+                    char_knowledge[text[0]]=text[1]
+            if(type=='loc'):
+                text = scrapeLoc(link)
+                if(text and text[1]):
+                    loc_knowledge[text[0]]=text[1]
+            if(type=='mag'):
+                text = scrapeLoc(link)
+                if(text and text[1]):
+                    mag_knowledge[text[0]]=text[1]
+            if(type=='life'):
+                text = scrapeLoc(link)
+                if(text and text[1]):
+                    life_knowledge[text[0]]=text[1]
+            if(type=='obj'):
+                text = scrapeLoc(link)
+                if(text and text[1]):
+                    obj_knowledge[text[0]]=text[1]
+            if(type=='era'):
+                text = scrapeLoc(link)
+                if(text and text[1]):
+                    era_knowledge[text[0]]=text[1]
+            if(type=='cult'):
+                text = scrapeLoc(link)
+                if(text and text[1]):
+                    cult_knowledge[text[0]]=text[1]
             
 
-scrape_characters()
-pickle.dump(knowledge,open('t_knowledgeBase.p','wb'))
+scrape_links('https://coppermind.net/wiki/Category:Characters','char')
+scrape_links('https://coppermind.net/wiki/Category:Locations','loc')
+scrape_links('https://coppermind.net/wiki/Category:Magic','mag')
+scrape_links('https://coppermind.net/wiki/Category:Lifeforms','life')
+scrape_links('https://coppermind.net/wiki/Category:Magical_entities','life')
+scrape_links('https://coppermind.net/wiki/Category:Objects_and_Materials','obj')
+scrape_links('https://coppermind.net/wiki/Category:Events_and_Eras','era')
+scrape_links('https://coppermind.net/wiki/Category:Culture','cult')
+pickle.dump(char_knowledge,open('knowledge/char_knowledgeBase.p','wb'))
+pickle.dump(loc_knowledge,open('knowledge/loc_knowledgeBase.p','wb'))
+pickle.dump(mag_knowledge,open('knowledge/mag_knowledgeBase.p','wb'))
+pickle.dump(life_knowledge,open('knowledge/life_knowledgeBase.p','wb'))
+pickle.dump(obj_knowledge,open('knowledge/obj_knowledgeBase.p','wb'))
+pickle.dump(era_knowledge,open('knowledge/era_knowledgeBase.p','wb'))
+pickle.dump(cult_knowledge,open('knowledge/cult_knowledgeBase.p','wb'))
+
+#print(era_knowledge['Battle of Alta'])
